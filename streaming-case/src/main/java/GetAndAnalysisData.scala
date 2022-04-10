e64e9f2e9b395f2a3c4f6700a072658eb6c576bc
Summer <34650347+wfy0508@users.noreply.github.com>
2022-04-10 22:35:18 +0800

    spark-streaming-ADAnalysis-20220410

--- streaming-case/src/main/java/GetAndAnalysisData.scala
+++ streaming-case/src/main/java/GetAndAnalysisData.scala
@@ -2,7 +2,7 @@ import com.sun.corba.se.pept.broker.Broker
 import com.sun.corba.se.spi.ior.ObjectKey
 import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord, KafkaConsumer}
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.kafka010.KafkaUtils.createDirectStream
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
@@ -23,6 +23,27 @@ object GetAndAnalysisData {
     val broker: String = "node1:9092"
     val topic: String = "sparkStreaming"
 
+    // 读取Kafka数据，创建DStream
+    val value: InputDStream[ConsumerRecord[String, String]] = readKafkaData(ssc, topic, broker)
+    // 拆解获取到的数据
+    val AdData: DStream[AdClickData] = value.map(
+      kafkaData => {
+        val data: String = kafkaData.value()
+        val strings: Array[String] = data.split(" ")
+        AdClickData(strings(0), strings(1), strings(2), strings(3), strings(4))
+      }
+    )
+
+    // TODO 周期性获取黑名单数据
+    // TODO 判断点击用户是否在黑名单中
+    // TODO 如果不在黑名单中，就进行数据统计
+
+    // TODO 如果统计后数据超过阈值，就将该用户加入黑名单
+    // TODO 如果统计后数据没有超过阈值，就更新当天点击数据
+    // TODO 更新后的点击数据是否超过阈值，超过就将该用户加入黑名单
+
+    ssc.start()
+    ssc.awaitTermination()
 
   }
 
@@ -44,8 +65,8 @@ object GetAndAnalysisData {
   /**
    * 读取Kafka数据，创建DStream
    *
-   * @param ssc StreamingContext
-   * @param topic Kafka主题
+   * @param ssc    StreamingContext
+   * @param topic  Kafka主题
    * @param broker 服务器
    * @return
    */
@@ -58,3 +79,6 @@ object GetAndAnalysisData {
   }
 
 }
+
+// 定义一个广告内容的样例类
+case class AdClickData(ts: String, area: String, city: String, user: String, ad: String)

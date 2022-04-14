360454f80a4c47a9c1baf3af3837aa511e3b8315
Summer <fayin1004@gmail.com>
2022-04-14 15:42:48 +0800

    learningSpark-SparkStreaming-JDBCUtil-20220414

--- streaming-case/src/main/java/JDBCUtil.scala
+++ streaming-case/src/main/java/JDBCUtil.scala
@@ -1,5 +1,6 @@
 import com.alibaba.druid.pool.DruidDataSourceFactory
-import java.sql.Connection
+
+import java.sql.{Connection, PreparedStatement}
 import java.util.Properties
 import javax.sql.DataSource
 
@@ -27,5 +28,52 @@ object JDBCUtil {
   def getConnection: Connection = {
     dataSource.getConnection
   }
+
+  // 插入一条记录
+  def executeUpdate(connection: Connection, sql: String, params: Array[Any]) = {
+    var rtn = 0
+    var ps: PreparedStatement = null
+    try {
+      connection.setAutoCommit(false)
+      ps = connection.prepareStatement(sql)
+      if (params != null && params.length > 0) {
+        for (i <- params.indices) {
+          ps.setObject(i + 1, params(i))
+        }
+      }
+      rtn = ps.executeUpdate()
+      connection.commit()
+      ps.close()
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+    rtn
+  }
+
+  // 批量插入数据
+  def executeBatchUpdate(connection: Connection, sql: String, paramsList: Iterable[Array[Any]]) = {
+    var rtn = 0
+    var ps: PreparedStatement = null
+    try {
+      connection.setAutoCommit(false)
+      ps = connection.prepareStatement(sql)
+      for (params <- paramsList) {
+        if (params != null && params.length > 0) {
+          for (i <- params.indices) {
+            ps.setObject(i + 1, params(i))
+          }
+          ps.addBatch()
+        }
+      }
+      rtn = ps.executeUpdate()
+      connection.commit()
+      ps.close()
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
+    rtn
+  }
+
+  // 判断一个数据是否存在
 
 }
